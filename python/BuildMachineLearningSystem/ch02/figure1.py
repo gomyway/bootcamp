d9ea1321295a7eedbdf5df0982327dd9efa03a58
gomyway <leotang2010@live.com>
2013-10-23 15:38:48 -0700

    add ch02 scripts

---
+++ python/BuildMachineLearningSystem/ch02/figure1.py
@@ -0,0 +1,29 @@
+# This code is supporting material for the book
+# Building Machine Learning Systems with Python
+# by Willi Richert and Luis Pedro Coelho
+# published by PACKT Publishing
+#
+# It is made available under the MIT License
+
+import numpy as np
+from sklearn.datasets import load_iris
+from matplotlib import pyplot as plt
+
+data = load_iris()
+features = data['data']
+feature_names = data['feature_names']
+target = data['target']
+
+#illustrate the feature space
+#visualization 
+pairs = [(0, 1), (0, 2), (0, 3), (1, 2), (1, 3), (2, 3)] #all the possible combination for two of the four features
+for i, (p0, p1) in enumerate(pairs):
+    plt.subplot(2, 3, i + 1)
+    for t, marker, c in zip(range(3), ">ox", "rgb"):
+        plt.scatter(features[target == t, p0], features[
+                    target == t, p1], marker=marker, c=c)
+    plt.xlabel(feature_names[p0])
+    plt.ylabel(feature_names[p1])
+    plt.xticks([])
+    plt.yticks([])
+plt.savefig('../1400_02_01.png')
